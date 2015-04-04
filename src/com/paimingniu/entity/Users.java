0b30921ba4d87c8084870c0b3d33ac968d4eefbf
coyun <coyun@qq.com>
2015-04-04 22:47:09 +0800

    Change-Id: Ibfba209e19dc390fd06626c7d21c53ebeb818f6c

---
+++ src/com/paimingniu/entity/Users.java
@@ -0,0 +1,193 @@
+package com.paimingniu.entity;
+
+import java.io.Serializable;
+
+/**
+ * @Description 描述
+ * @author 杨克运
+ * @email coyun@qq.com
+ * @date 2015年1月5日 下午2:28:27
+ * @version V1.0
+ */
+public class Users implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private long id;
+	private String email;
+	private String password;
+	private String tname;// 旺旺名称
+	private String mac;
+	private String ip;//
+	private long registTimes;// 注册时间
+	private int status;// 状态
+	private int type;// 用户类型
+	private long refId;// 推荐ID
+
+	private String field;// 散列值
+
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the tname
+	 */
+	public String getTname() {
+		return tname;
+	}
+
+	/**
+	 * @param tname
+	 *            the tname to set
+	 */
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+
+	/**
+	 * @return the mac
+	 */
+	public String getMac() {
+		return mac;
+	}
+
+	/**
+	 * @param mac
+	 *            the mac to set
+	 */
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+
+	/**
+	 * @return the ip
+	 */
+	public String getIp() {
+		return ip;
+	}
+
+	/**
+	 * @param ip
+	 *            the ip to set
+	 */
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+
+	/**
+	 * @return the registTimes
+	 */
+	public long getRegistTimes() {
+		return registTimes;
+	}
+
+	/**
+	 * @param registTimes
+	 *            the registTimes to set
+	 */
+	public void setRegistTimes(long registTimes) {
+		this.registTimes = registTimes;
+	}
+
+	/**
+	 * @return the status
+	 */
+	public int getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param status
+	 *            the status to set
+	 */
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public int getType() {
+		return type;
+	}
+
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the refId
+	 */
+	public long getRefId() {
+		return refId;
+	}
+
+	/**
+	 * @param refId
+	 *            the refId to set
+	 */
+	public void setRefId(long refId) {
+		this.refId = refId;
+	}
+
+	/**
+	 * @return the field
+	 */
+	public String getField() {
+		return field;
+	}
+
+	/**
+	 * @param field
+	 *            the field to set
+	 */
+	public void setField(String field) {
+		this.field = field;
+	}
+
+}
